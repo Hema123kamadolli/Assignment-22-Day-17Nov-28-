69e159b21400b726b1f54a5ce95d9b14ad97642c
Hema123kamadolli <hema.kamadolli@mphasis.com>
2023-11-28 15:02:19 +0530

    Assignment-22

---
+++ Library.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_22
+{
+    public class Library
+    {
+        private List<Book> bookList = new List<Book>();
+
+        public void AddBook(int bookId, string title, string author, string genre)
+        {
+            Book newBook = new Book
+            {
+                BookId = bookId,
+                Title = title,
+                Author = author,
+                Genre = genre,
+                IsAvailable = true
+            };
+
+            bookList.Add(newBook);
+            Console.WriteLine("Book added successfully.");
+        }
+
+        public void ViewAllBooks()
+        {
+            Console.WriteLine("Library Catalog:");
+            foreach (var book in bookList)
+            {
+                Console.WriteLine($"ID: {book.BookId}, Title: {book.Title}, Author: {book.Author}, Genre: {book.Genre}, Available: {(book.IsAvailable ? "Yes" : "No")}");
+            }
+        }
+
+        public void SearchBookById(int bookId)
+        {
+            Book foundBook = bookList.Find(book => book.BookId == bookId);
+            if (foundBook != null)
+            {
+                Console.WriteLine("Book found:");
+                Console.WriteLine($"ID: {foundBook.BookId}, Title: {foundBook.Title}, Author: {foundBook.Author}, Genre: {foundBook.Genre}, Available: {(foundBook.IsAvailable ? "Yes" : "No")}");
+            }
+            else
+            {
+                Console.WriteLine($"Book not found with ID: {bookId}");
+            }
+        }
+
+        public void SearchBookByTitle(string title)
+        {
+            Book foundBook = bookList.Find(book => book.Title.Equals(title, StringComparison.OrdinalIgnoreCase));
+            if (foundBook != null)
+            {
+                Console.WriteLine("Book found:");
+                Console.WriteLine($"ID: {foundBook.BookId}, Title: {foundBook.Title}, Author: {foundBook.Author}, Genre: {foundBook.Genre}, Available: {(foundBook.IsAvailable ? "Yes" : "No")}");
+            }
+            else
+            {
+                Console.WriteLine($"Book not found with title: {title}");
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+namespace Assignment_22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Library library = new Library();
+            int choice;
+
+            do
+            {
+                Console.WriteLine("Library Management System");
+                Console.WriteLine("1. Add a book");
+                Console.WriteLine("2. View all books");
+                Console.WriteLine("3. Search by ID");
+                Console.WriteLine("4. Search by title");
+                Console.WriteLine("5. Exit");
+                Console.Write("Enter your choice: ");
+                int.TryParse(Console.ReadLine(), out choice);
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.Write("Enter Book ID: ");
+                        int bookId = int.Parse(Console.ReadLine());
+                        Console.Write("Enter book title: ");
+                        string title = Console.ReadLine();
+                        Console.Write("Enter author: ");
+                        string author = Console.ReadLine();
+                        Console.Write("Enter genre: ");
+                        string genre = Console.ReadLine();
+
+                        library.AddBook(bookId, title, author, genre);
+                        break;
+
+                    case 2:
+                        library.ViewAllBooks();
+                        break;
+
+                    case 3:
+                        Console.Write("Enter book ID to search: ");
+                        int searchId = int.Parse(Console.ReadLine());
+                        library.SearchBookById(searchId);
+                        break;
+
+                    case 4:
+                        Console.Write("Enter book title to search: ");
+                        string searchTitle = Console.ReadLine();
+                        library.SearchBookByTitle(searchTitle);
+                        break;
+
+                    case 5:
+                        Console.WriteLine("Exiting program. Goodbye!");
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+                }
+
+            } while (choice != 5);
+
+        }
+    }
+}
